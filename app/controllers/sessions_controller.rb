3812bb55bf749a3c1f738c6d1560fb3a7f0f5c45
Divyanshu Mehta <divyanshumehta@outlook.com>
2018-03-04 09:04:53 +0530

    Check for unique email implemented

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,6 +12,11 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.where(:provider => auth['provider'],
                       :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
+    if User.where(email:user.email).count > 1
+      user.destroy
+      redirect_to root_url, :alert => "User account with same email already exists"
+      return
+    end
     reset_session
     session[:user_id] = user.id
     redirect_to root_url, :notice => 'Signed in!'
